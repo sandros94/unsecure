d751b87eeb1c54ab956179577a8d163f87adf39c
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-06-22 16:49:10 +0200

    chore: remove `*.d.ts` from final build

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,6 @@
 import { defineBuildConfig } from "unbuild";
+import { readdir, rm } from "node:fs/promises";
+import { join } from "node:path";
 
 export default defineBuildConfig({
   entries: [
@@ -10,7 +12,27 @@ export default defineBuildConfig({
     },
   ],
   declaration: true,
-  rollup: {
-    emitCJS: false,
+  hooks: {
+    async "build:done"() {
+      await removeDtsFiles("dist");
+    },
   },
 });
+
+async function removeDtsFiles(directory: string) {
+  try {
+    const items = await readdir(directory, { recursive: true });
+    for (const item of items) {
+      const itemPath = join(directory, item);
+
+      if (item.endsWith(".d.ts")) {
+        await rm(itemPath);
+      }
+    }
+  } catch (error) {
+    if (error.code === "ENOENT" || error.code === "ENOTDIR") {
+      return;
+    }
+    console.error(`Error processing ${directory}: ${error}`);
+  }
+}
